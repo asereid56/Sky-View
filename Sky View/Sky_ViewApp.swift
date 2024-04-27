2c061e0146f0ceb35444230efd0b3d9d2a804dc3
Aser Eid <asereid56@gmail.com>
2024-04-27 21:32:30 +0300

    Initial Commit

---
+++ Sky View/Sky_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sky_ViewApp.swift
+//  Sky View
+//
+//  Created by Aser Eid on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sky_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
